dd00523f4bf5f7c9005ee10ba7896fa64d23525a
Matthew Haigh <mlhaigh80@gmail.com>
2016-11-13 11:34:24 -0500

    fixed packet size

--- iprules.sh
+++ iprules.sh
@@ -3,5 +3,5 @@ sysctl -p
 
 ip rule add fwmark 1 lookup 100
 ip route add local 0.0.0.0/0 dev lo table 100
-iptables -t mangle -A PREROUTING -p udp -j TPROXY --tproxy-mark 0x1/0x1 --on-port 7777
+iptables -t mangle -A PREROUTING -p udp --dport 8888 -j TPROXY --tproxy-mark 0x1/0x1 --on-port 7777
 
